aa4e5ab40e3b8afc83d09d6af1623467177cc673
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2025-02-09 01:35:44 -0600

    fix(Hero): Eslint rules fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
         <h3 className='text-2xl font-medium'>Hello!</h3>
         <div className='flex flex-col md:flex-row items-center gap-x-4'>
           <div className='flex items-center gap-x-4 w-full md:w-auto'>
-            <h3 className='text-5xl xl:text-7xl font-medium'>I'm </h3>
+            <h3 className='text-5xl xl:text-7xl font-medium'>I&apos;m </h3>
             <Image
               src={'/myself-emoji.svg'}
               alt={'Alejandro Villalobos'}
@@ -30,7 +30,7 @@ function Hero() {
             />
           </div>
         </div>
-        <p className=' text-3xl xl:text-6xl font-medium leading-tight'>I’m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
+        <p className=' text-3xl xl:text-6xl font-medium leading-tight'>I&apos;m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
       </div>
     </div>
   )
