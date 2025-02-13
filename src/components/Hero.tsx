f52117f97aab6d47f4c995318de23eb75c3ad210
Villalobos14 <joshuaalejandrovillalobosestra@gmail.com>
2025-02-12 21:21:15 -0600

    feat(componentes) hero: color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='h-screen flex flex-col items-center px-8 xl:px-20 pt-20'>
+    <div className='h-screen flex flex-col items-center px-8 xl:px-20 pt-20 bg-[#0d0d0d]'>
       <div className='w-full flex flex-col gap-y-8'>
-        <h3 className='text-4xl font-medium '>Hello!</h3>
+        <h3 className='text-white text-4xl font-medium '>Hello!</h3>
         <div className='flex flex-col md:flex-row items-center gap-x-4'>
           <div className='flex items-center gap-x-4 w-full md:w-auto'>
-            <h3 className='text-5xl xl:text-7xl font-medium'>I&apos;m </h3>
+            <h3 className='text-white text-5xl xl:text-7xl font-medium'>I&apos;m </h3>
             <Image
               src={'/myself-emoji.svg'}
               alt={'Alejandro Villalobos'}
@@ -19,7 +19,7 @@ function Hero() {
             />
           </div>
           <div className='w-full md:w-auto flex items-center gap-x-4'>
-            <h3 className='text-5xl xl:text-7xl font-medium'>Alejandro VIllalobos</h3>
+            <h3 className='text-white text-5xl xl:text-7xl font-medium'>Alejandro VIllalobos</h3>
             <Image
               src={'/myself-emoji.svg'}
               alt={'Alejandro Villalobos'}
@@ -30,7 +30,7 @@ function Hero() {
             />
           </div>
         </div>
-        <p className='text-3xl xl:text-6xl font-medium leading-tight'>I&apos;m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
+        <p className='text-white text-3xl xl:text-6xl font-medium leading-tight'>I&apos;m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
       </div>
     </div>
   )
