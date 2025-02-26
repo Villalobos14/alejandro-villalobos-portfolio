a27be2488c32c5c82030d8cdf9bf6a6b0a08a1c2
Villalobos14 <joshuaalejandrovillalobosestra@gmail.com>
2025-02-26 00:11:21 -0600

    joshua

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,10 @@ function Hero() {
   return (
     <main className='h-screen flex flex-col items-center px-8 xl:px-20 pt-20'>
       <div className='w-full flex flex-col gap-y-8'>
-        <h3 className='text-white text-4xl font-medium '>HEllo!</h3>
+        <h3 className=' text-4xl font-medium '>Hello!</h3>
         <div className='flex flex-col md:flex-row items-center gap-x-4'>
           <div className='flex items-center gap-x-4 w-full md:w-auto'>
-            <h3 className='text-white text-5xl xl:text-7xl font-medium'>I&apos;m </h3>
+            <h3 className=' text-5xl xl:text-7xl font-medium'>I&apos;m </h3>
             <Image
               src={'/myself-emoji.svg'}
               alt={'Alejandro Villalobos'}
@@ -19,7 +19,7 @@ function Hero() {
             />
           </div>
           <div className='w-full md:w-auto flex items-center gap-x-4'>
-            <h3 className='text-white text-5xl xl:text-7xl font-medium'>Alejandro VIllalobos</h3>
+            <h3 className='text-5xl xl:text-7xl font-medium'>Alejandro VIllalobos</h3>
             <Image
               src={'/myself-emoji.svg'}
               alt={'Alejandro Villalobos'}
@@ -30,7 +30,7 @@ function Hero() {
             />
           </div>
         </div>
-        <p className='text-white text-3xl xl:text-6xl font-medium leading-tight'>I&apos;m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
+        <p className=' text-3xl xl:text-6xl font-medium leading-tight'>I&apos;m UX/UI Designer and I create a digital experience that borders on efficiency, aesthetics and functionality. 🚀</p>
       </div>
     </main>
   )
