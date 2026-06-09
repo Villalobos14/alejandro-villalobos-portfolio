021d8a24a8fd347827f2db95275f502f58815d2c
Villalobos14 <villalobosestrada222@gmail.com>
2026-06-08 18:13:44 -0600

    progresssss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import About from "@/components/About";
-import Experience from "@/components/Experience";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+
 import SmoothSection from "@/components/SmoothSection";
 
 export default function Home() {
